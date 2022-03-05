fbcd5813d22bf1e555dfba24cfec6a5dd161dd8b
Daniel Santana Santos <gc04346@gmail.com>
2022-03-05 16:56:36 -0300

    instalando bottom tab navigator

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,8 +2,8 @@ import {Container, Title} from "./styles";
 
 export default function Main () {
     return (
-        <Container>
-            <Title>Iniciando projeto Finance Manager!</Title>
-        </Container>
-    )
+      <Container>
+        <Title>Iniciando projeto Finance Manager!</Title>
+      </Container>
+    );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Main from "./pages/Main";
+import RegisterExpense from './pages/RegisterExpense';
 
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="New" component={RegisterExpense} />
+      <Tab.Screen name="Home" component={Main} />
+    </Tab.Navigator>
+  );
+}
 
 const Routes = () => {
     return (
         <NavigationContainer>
-            <Main/>
+            <MyTabs/>
         </NavigationContainer>
-    )
+    );
 }
 
 export default Routes;
