41b7f9c2bb1f1e24f2ae65b7e0c6ed8b3b8aa04b
Daniel Santana Santos <gc04346@gmail.com>
2022-03-07 19:14:03 -0300

    checkpoint pre-sombras

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,31 +4,64 @@ import { FontAwesome } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   tabBar: {
+    position: "relative",
     flexDirection: "row",
     backgroundColor: "#42bc3f",
-    borderRadius: 12,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
     maxHeight: 70,
   },
   tabIcon: {
+    position: "relative",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   container: {
+    position: "relative",
+    height: 70,
+    width: 70,
+    zIndex: 1
+  },
+  indicator: {
+    position: "absolute",
     height: 70,
     width: 70,
     backgroundColor: "#33FFA2",
-    zIndex: 1
+    top: "-50%",
+    borderRadius: 35,
+    borderColor: "#FFF",
+    borderWidth: 6,
+    borderStyle: "solid",
+  },
+  indicatorBefore: {
+    position: "absolute",
+    left: -12,
+    width: 15,
+    height: 15,
+    backgroundColor: "red",
+    borderTopRightRadius: 20,
+
+    shadowColor: "#FFF",
+    shadowOffset: {width: -30, height: -30},
+    shadowOpacity: 0,
+    shadowRadius: 30,
+    elevation: 1
+    
+  },
+  indicatorAfter: {
+
   },
   icon: {
     color: "#222",
     fontSize: 28,
     marginTop: 20,
-    marginBottom: -20,
+    marginBottom: -10,
   },
   text: {
     color: "#222",
-    marginBottom: 20,
+    marginBottom: 10,
+    fontWeight: "bold",
   }
 });
 
@@ -71,22 +104,33 @@ export default function NavBar({ state, descriptors, navigation }) {
         };
 
         return (
-          <View style={styles.container}>
-          <TouchableOpacity
-          key={`nav-btn-${index}`}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={styles.tabIcon}
-          >
-            <FontAwesome name={navIcons[route.name]} style={{...styles.icon, transform:[{translateY:isFocused ? -35 : 0}]}} />
-            <Text style={{ ...styles.text, opacity: isFocused ? 1 : 0}}>
-              {label}
-            </Text>
-          </TouchableOpacity>
+          <View key={`nav-btn-${index}`} style={{marginLeft: 20}}>
+            {/* Quadradinho verde atrás do ícone */}
+            <View style={isFocused && styles.indicator} />
+            <View style={styles.container}>
+              <View style={styles.indicatorBefore}/>
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                style={styles.tabIcon}
+              >
+                <FontAwesome
+                  name={navIcons[route.name]}
+                  style={{
+                    ...styles.icon,
+                    transform: [{ translateY: isFocused ? -35 : 0 }],
+                  }}
+                />
+                <Text style={{ ...styles.text, opacity: isFocused ? 1 : 0 }}>
+                  {label}
+                </Text>
+              </TouchableOpacity>
+              <View style={styles.indicatorAfter}/>
+            </View>
           </View>
         );
       })}
