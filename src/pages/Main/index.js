3fae3c6aaaa1ff57a086e49f0e6a11d2f3e1a964
Daniel Santana Santos <gc04346@gmail.com>
2022-03-15 19:32:58 -0300

    mais algum progresso na tela principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,17 @@
-import {Text} from 'react-native';
-import { ProgressBar } from 'react-native-paper';
-import { Container, Title, Top, PresentExpenses, ExpenseDescription, ExpenseProgressBar, Expense } from "./styles";
+import { ScrollView, Text } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import {
+  Container,
+  Title,
+  Top,
+  PresentExpenses,
+  FutureExpenses,
+  ExpenseDescription,
+  ExpenseProgressBar,
+  Expense,
+  ExpenseCategory,
+  ExpenseCategoryTitle,
+} from "./styles";
 
 export default function Main() {
   return (
@@ -8,40 +19,179 @@ export default function Main() {
       <Top>
         <Title>Março</Title>
       </Top>
-      <PresentExpenses>
-        <Expense>
-        <ExpenseDescription>Gastos</ExpenseDescription>
-        <ExpenseProgressBar>
-          <ProgressBar progress={0.15} color="#C70039" styles={{width: '30px', borderRadius:5}}/>
-          <ExpenseDescription>$750 / $5.000</ExpenseDescription>
-        </ExpenseProgressBar>
-        <ExpenseDescription>(15%)</ExpenseDescription>
-        </Expense>
-        <Expense>
-        <ExpenseDescription>Gastos</ExpenseDescription>
-        <ExpenseProgressBar>
-          <ProgressBar progress={0.15} color="#C70039" styles={{width: '30px', borderRadius:5}}/>
-          <ExpenseDescription>$750 / $5.000</ExpenseDescription>
-        </ExpenseProgressBar>
-        <ExpenseDescription>(15%)</ExpenseDescription>
-        </Expense>
-        <Expense>
-        <ExpenseDescription>Gastos</ExpenseDescription>
-        <ExpenseProgressBar>
-          <ProgressBar progress={0.15} color="#C70039" styles={{width: '30px', borderRadius:5}}/>
-          <ExpenseDescription>$750 / $5.000</ExpenseDescription>
-        </ExpenseProgressBar>
-        <ExpenseDescription>(15%)</ExpenseDescription>
-        </Expense>
-        <Expense>
-        <ExpenseDescription>Gastos</ExpenseDescription>
-        <ExpenseProgressBar>
-          <ProgressBar progress={0.15} color="#C70039" styles={{width: '30px', borderRadius:5}}/>
-          <ExpenseDescription>$750 / $5.000</ExpenseDescription>
-        </ExpenseProgressBar>
-        <ExpenseDescription>(15%)</ExpenseDescription>
-        </Expense>
-      </PresentExpenses>
+      <ScrollView>
+      <ExpenseCategory>
+          <ExpenseCategoryTitle style={{ transform: [{ rotate: "-90deg" }] }}>
+            Present (70%)
+          </ExpenseCategoryTitle>
+          <PresentExpenses>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#C70039"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+          </PresentExpenses>
+        </ExpenseCategory>
+        
+        <ExpenseCategory>
+          <ExpenseCategoryTitle style={{ transform: [{ rotate: "-90deg" }] }}>
+            Futuro (30%)
+          </ExpenseCategoryTitle>
+          <FutureExpenses>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#581845"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#581845"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#581845"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+            <Expense>
+              <ExpenseDescription>Gastos</ExpenseDescription>
+              <ExpenseProgressBar>
+                <ProgressBar
+                  progress={0.15}
+                  color="#581845"
+                  styles={{ width: "30px", borderRadius: 5 }}
+                />
+                <ExpenseDescription>$750 / $5.000</ExpenseDescription>
+              </ExpenseProgressBar>
+              <ExpenseDescription>(15%)</ExpenseDescription>
+            </Expense>
+          </FutureExpenses>
+        </ExpenseCategory>
+      </ScrollView>
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -7,6 +7,11 @@ export const Container = styled.View`
   padding: 40px 0 30px;
 `;
 
+export const ExpenseCategory = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
 export const Top = styled.View`
   flex-direction: row;
   align-items: center;
@@ -20,13 +25,28 @@ export const Title = styled.Text`
   font-weight: bold;
 `;
 
+export const ExpenseCategoryTitle = styled.Text`
+  color: #000;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
 export const PresentExpenses = styled.ScrollView.attrs({
   contentContainerStyle: {
     backgroundColor: "#b4d8d8",
     minHeight: "50%",
+    maxHeight: "50%",
+    borderRadius: 15,
+  },
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const FutureExpenses = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    backgroundColor: "#DAF7A6",
+    minHeight: "50%",
     minWidth: "90%",
-    flexDirection: "column",
-    justifyContent: "space-around",
     borderRadius: 15,
   },
   showsHorizontalScrollIndicator: false,
@@ -34,7 +54,9 @@ export const PresentExpenses = styled.ScrollView.attrs({
 
 
 export const Expense = styled.View`
-  margin: 5px;
+  margin: 10px;
+  flex-direction: row;
+  justify-content: space-around;
 `;
 
 export const ExpenseProgressBar = styled.View`
